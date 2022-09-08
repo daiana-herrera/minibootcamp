b92797348de0f988c72302c4256061cc09943fae
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-09-08 10:56:38 -0300

    sec9 finish inject services

--- Training/services-app/src/app/account/account.component.ts
+++ Training/services-app/src/app/account/account.component.ts
@@ -1,20 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+import { AccountsService } from '../account.service';
 import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+ 
 
-  constructor(private loggingService : LoggingService) {}
+  constructor(private loggingService : LoggingService,
+              private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-   this.loggingService.logStatusChange(status);
+    this.accountsService.updateStatus(this.id, status);
+  // this.loggingService.logStatusChange(status);
+   this.accountsService.statusUpdated.emit(status); // added in account service,  and i want listen new.-account
   }
 }
--- Training/services-app/src/app/new-account/new-account.component.ts
+++ Training/services-app/src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { SubscriptionLoggable } from 'rxjs/internal/testing/SubscriptionLoggable';
+import { Component} from '@angular/core';
+import { AccountsService } from '../account.service';
+
 import { LoggingService } from '../logging.service';
 
 @Component({
@@ -7,21 +8,25 @@ import { LoggingService } from '../logging.service';
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
   //porvider a service, add and specify the type :
-  providers: [LoggingService]
+  //providers: [LoggingService] coment because we add it in the app.moule.ts
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
 //So how do we inform Angular that we require 
     //such an instance? We add a constructor to the class,
     // to the component 
-   constructor(private loggingService : LoggingService) {}
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+   constructor(private loggingService : LoggingService, 
+               private accountsService: AccountsService) {
+                this.accountsService.statusUpdated.subscribe(
+                  (status: string)=> alert('New Status:' + status)//cross component comunication
+                ); //for listen 
+               }
+  
+               onCreateAccount(accountName: string, accountStatus: string) {
+   
     //last stpep,add instance:
-    this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
 
    
   }
