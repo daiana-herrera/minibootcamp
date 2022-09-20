a86af84bc21768f2b61a1de160b020c694a9041e
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-09-20 12:07:14 -0300

    server config finish secc11 - error .then

---
+++ Training/routing-app/src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -0,0 +1,18 @@
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Observable } from "rxjs";
+
+export interface CanComponentDeactivate {
+    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+
+
+}
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+    canDeactivate(component: CanComponentDeactivate,
+        CurrentRoute: ActivatedRouteSnapshot,
+        currentState: RouterStateSnapshot,
+        nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> |
+        boolean {
+        return component.canDeactivate();
+    }
+}    
