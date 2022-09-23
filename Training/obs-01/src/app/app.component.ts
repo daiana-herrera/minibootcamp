8381c61275be772a99ad99909d8313e1081a5868
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-09-23 10:30:10 -0300

    seccion 13 finished.Obs-01

--- Training/obs-01/src/app/app.component.ts
+++ Training/obs-01/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgModule, SchemaMetadata } from '@angular/core';
+import { UserService } from './user.service';
+import {Subscription} from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
 
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy, NgModule {
+  schemas?: (any[ 'NO_ERRORS_SCHEMA'] | SchemaMetadata)[]; //I add this seraching an error, to fix. but deploy well whithout Ngmodule non.
+userActivated = true;
+private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe (didActivate => {
+      this.userActivated = didActivate;
+    })
+  
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+    
+  }
 }
