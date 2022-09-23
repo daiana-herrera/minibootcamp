91597ed0f7fd9d77b07443ef77034545e19fe21d
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-09-23 10:58:54 -0300

    seccion14 finished in recipie-appstrict

--- Training/recipe-appstrict/src/app/shopping-list/shopping-list.component.ts
+++ Training/recipe-appstrict/src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs-compat'; // the program add rxjs-compat with subscription-
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.subscription = this.slService.ingredientsChanged //when i defined this, compile with next. no error founded.
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
