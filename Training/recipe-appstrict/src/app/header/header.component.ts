c0ced330bb92d0764f42bff890522f8c26d92795
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-10-02 15:52:30 -0300

    storing recipes

--- Training/recipe-appstrict/src/app/header/header.component.ts
+++ Training/recipe-appstrict/src/app/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { DataStorangeService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+constructor(private dataStorageService: DataStorangeService){}
+
+  onSaveData(){
+    this.dataStorageService.storeRecipes();
+  }
 }
