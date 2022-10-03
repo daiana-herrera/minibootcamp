cd4cb161534a291ce374e9eb083de0f0666633c0
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-10-03 16:55:50 -0300

    seccion 21 finished - components

--- Training/recipe-appstrict/src/app/app.module.ts
+++ Training/recipe-appstrict/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loadng-spinner.component';
 import { AuthInterceptorService } from './auth/auth.interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 
 @NgModule({
@@ -37,6 +39,8 @@ import { AuthInterceptorService } from './auth/auth.interceptor.service';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective,
   ],
   imports: [
     BrowserModule,
@@ -51,6 +55,9 @@ import { AuthInterceptorService } from './auth/auth.interceptor.service';
      {provide: HTTP_INTERCEPTORS, 
       useClass: AuthInterceptorService,
        multi: true}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule { }
