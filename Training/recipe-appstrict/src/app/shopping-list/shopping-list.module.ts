e0c9da458ce0ff11e41db844c148a4648a9caff7
DaianaHerrera <daiana.a.herrera@gmail.com>
2022-10-04 09:07:21 -0300

    finish secc 22

--- Training/recipe-appstrict/src/app/shopping-list/shopping-list.module.ts
+++ Training/recipe-appstrict/src/app/shopping-list/shopping-list.module.ts
@@ -12,7 +12,7 @@ import { SharedModule } from '../shared/shared.module';
     imports: [
         FormsModule,
         RouterModule.forChild([
-            { path: 'shopping-list',
+            { path: '',
             component: ShoppingListComponent},
         ]),
         SharedModule
